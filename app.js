701df8088912273b75c8ebee1f1f051cb0902c5d
Sergei <sepolish@student.jyu.fi>
2019-09-18 17:18:27 +0300

    add models/blog.js

--- app.js
+++ app.js
@@ -4,16 +4,9 @@ const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
+const Blog = require('./models/blog')
 
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
   .then(result => {
