4e609f6c1a918bd1745cacfb958d2171c37e0218
Sergei <sepolish@student.jyu.fi>
2019-09-18 17:10:24 +0300

    add app.js

---
+++ app.js
@@ -0,0 +1,47 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
+  .then(result => {
+    console.log('yhteys tietokantaan saatu')
+  })
+  .catch(err => {
+    console.log('virhe yhteysotossa tietokantaan', err.message)
+  })
+
+app.use(cors())
+app.use(bodyParser.json())
+
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+module.exports = app
--- index.js
+++ index.js
@@ -1,50 +1,9 @@
-// const http = require('http')
+const http = require('http')
 const config = require('./utils/config')
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const mongoose = require('mongoose')
+const app = require('./app')
 
+const server = http.createServer(app)
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
-  .then(result => {
-    console.log('yhteys tietokantaan saatu')
-  })
-  .catch(err => {
-    console.log('virhe yhteysotossa tietokantaan', err.message)
-  })
-
-app.use(cors())
-app.use(bodyParser.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-app.listen(config.PORT, () => {
+server.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`)
 })
