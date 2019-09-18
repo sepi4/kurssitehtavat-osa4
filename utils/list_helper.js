2bf532aaae392d6c490c969c070081cf24ad8afc
Sergei <sepolish@student.jyu.fi>
2019-09-19 02:33:52 +0300

    4.5*: apufunktioita ja yksikkötestejä, step3

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -9,7 +9,48 @@ const totalLikes = (blogs) => {
   )
 }
 
+const favoriteBlog = blogs => {
+  const blog = blogs.reduce(
+    (previous, current) => {
+      if (previous === undefined) {
+        return current
+      }
+      if (previous.likes > current.likes) {
+        return previous
+      }
+      else {
+        return current
+      }
+    },
+    undefined
+  )
+  return blog !== undefined
+    ? {
+      title: blog.title,
+      author: blog.author,
+      likes: blog.likes
+    }
+    : blog
+}
+
+// const mostBlogs = blogs => {
+//   const objAuthors = {}
+//   blogs.forEach(a => {
+//     if (objAuthors[a.author] === undefined) {
+//       objAuthors[a.author] = 1
+//     }
+//     else {
+//       objAuthors[a.author] += 1
+//     }
+//   })
+  
+//   Object.keys(objAuthors)
+//   return objAuthors
+// }
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
+  // mostBlogs,
 }
