158b934bedfd991d95e1525f10011316e285570e
Sergei <sepolish@student.jyu.fi>
2019-09-18 17:28:18 +0300

    add controllers/blogs.js

--- app.js
+++ app.js
@@ -4,9 +4,8 @@ const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
-const Blog = require('./models/blog')
 
-
+const blogsRouter = require('./controllers/blogs')
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
   .then(result => {
@@ -19,22 +18,6 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
 app.use(cors())
 app.use(bodyParser.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('', blogsRouter)
 
 module.exports = app
