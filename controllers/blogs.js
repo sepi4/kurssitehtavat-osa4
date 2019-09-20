513dfee47362a1e9e7e04862a529e6b1583f2b6d
Sergei <sepolish@student.jyu.fi>
2019-09-20 04:57:10 +0300

    add trycatch and add all tests back

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -5,33 +5,27 @@ const Blog = require('../models/blog')
 blogsRouter.get('/', async (request, response) => {
   const allBlogs = await Blog.find({})
   response.json(allBlogs.map(b => b.toJSON()))
-
-  // Blog
-  //   .find({})
-  //   .then(blogs => {
-  //     response.json(blogs.map(b => b.toJSON()))
-  //   })
 })
 
-blogsRouter.post('/', async (request, response) => {
-  const newBlog = request.body
+blogsRouter.post('/', async (request, response, next) => {
+  try {
+    const newBlog = request.body
 
-  if (typeof newBlog.likes !== 'number') {
-    newBlog.likes = 0
+    if (typeof newBlog.likes !== 'number') {
+      newBlog.likes = 0
+    }
+    if (typeof newBlog.url !== 'string' || typeof newBlog.title !== 'string') {
+      response.status(400).end()
+      return
+    }
+
+    const blog = new Blog(newBlog)
+    const savedBlog = await blog.save()
+    response.status(201).json(savedBlog.toJSON())
   }
-  if (typeof newBlog.url !== 'string' || typeof newBlog.title !== 'string') {
-    response.status(400).end()
-    return
+  catch (exception) {
+    next(exception)
   }
-
-  const blog = new Blog(newBlog)
-  const savedBlog = await blog.save()
-  response.status(201).json(savedBlog.toJSON())
-
-  // .save()
-  // .then(savedBlog => {
-  //   response.status(201).json(savedBlog.toJSON())
-  // })
 })
 
 blogsRouter.delete('/:id', async (request, response, next) => {
@@ -39,14 +33,20 @@ blogsRouter.delete('/:id', async (request, response, next) => {
   try {
     await Blog.findByIdAndRemove(id)
     response.status(204).end()
-  } catch (exception) {
+  }
+  catch (exception) {
     next(exception)
   }
 })
 
 blogsRouter.put('/:id', async (request, response, next) => {
-  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, request.body, { new: true })
-  response.json(updatedBlog.toJSON())
+  try {
+    const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, request.body, { new: true })
+    response.json(updatedBlog.toJSON())
+  }
+  catch (exception) {
+    next(exception)
+  }
 })
 
 module.exports = blogsRouter
